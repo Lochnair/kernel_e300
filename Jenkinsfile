Lochnair/kernel_e200

Fix wrong filenames

Jenkinsfile
@@ -20,7 +20,7 @@ pipeline {
 
         stage('Prepare') {
             steps {
-                sh 'make ARCH=mips ubnt_er_e100_defconfig'
+                sh 'make ARCH=mips ubnt_er_e200_defconfig'
             }
         }
 
@@ -28,8 +28,8 @@ pipeline {
             steps {
                 sh 'make -j5 ARCH=mips CROSS_COMPILE=mips64-octeon-linux- prepare modules_prepare'
                 sh 'rm -rf tmp && mkdir tmp'
-                sh 'tar --exclude-vcs --exclude=tmp -cf tmp/e100-ksrc.tar .'
-                sh 'mv tmp/e100-ksrc.tar e100-ksrc.tar'
+                sh 'tar --exclude-vcs --exclude=tmp -cf tmp/e200-ksrc.tar .'
+                sh 'mv tmp/e200-ksrc.tar e200-ksrc.tar'
             }
         }
 
@@ -42,24 +42,24 @@ pipeline {
         stage('Archive kernel image') {
             steps {
                 sh 'mv -v vmlinux vmlinux.64'
-                sh 'tar cvjf e100-kernel.tar.bz2 vmlinux.64'
-                archiveArtifacts artifacts: 'e100-kernel.tar.bz2', fingerprint: true, onlyIfSuccessful: true
+                sh 'tar cvjf e200-kernel.tar.bz2 vmlinux.64'
+                archiveArtifacts artifacts: 'e200-kernel.tar.bz2', fingerprint: true, onlyIfSuccessful: true
             }
         }
         
         stage('Archive kernel modules') {
             steps {
                 sh 'make ARCH=mips CROSS_COMPILE=mips64-octeon-linux- INSTALL_MOD_PATH=destdir modules_install'
-                sh 'tar cvjf e100-modules.tar.bz2 -C destdir .'
-                archiveArtifacts artifacts: 'e100-modules.tar.bz2', fingerprint: true, onlyIfSuccessful: true
+                sh 'tar cvjf e200-modules.tar.bz2 -C destdir .'
+                archiveArtifacts artifacts: 'e200-modules.tar.bz2', fingerprint: true, onlyIfSuccessful: true
             }
         }
 
         stage('Archive build tree') {
             steps {
-                sh 'tar -uvf e100-ksrc.tar Module.symvers'
-                sh 'bzip2 e100-ksrc.tar'
-                archiveArtifacts artifacts: 'e100-ksrc.tar.bz2', fingerprint: true, onlyIfSuccessful: true
+                sh 'tar -uvf e200-ksrc.tar Module.symvers'
+                sh 'bzip2 e200-ksrc.tar'
+                archiveArtifacts artifacts: 'e200-ksrc.tar.bz2', fingerprint: true, onlyIfSuccessful: true
             }
         }
     }

Lochnair/kernel_e300

Jenkinsfile
@@ -20,7 +20,7 @@ pipeline {
 
         stage('Prepare') {
             steps {
-                sh 'make ARCH=mips ubnt_er_e100_defconfig'
+                sh 'make ARCH=mips ubnt_er_e300_defconfig'
             }
         }
 
@@ -28,8 +28,8 @@ pipeline {
             steps {
                 sh 'make -j5 ARCH=mips CROSS_COMPILE=mips64-octeon-linux- prepare modules_prepare'
                 sh 'rm -rf tmp && mkdir tmp'
-                sh 'tar --exclude-vcs --exclude=tmp -cf tmp/e100-ksrc.tar .'
-                sh 'mv tmp/e100-ksrc.tar e100-ksrc.tar'
+                sh 'tar --exclude-vcs --exclude=tmp -cf tmp/e300-ksrc.tar .'
+                sh 'mv tmp/e300-ksrc.tar e300-ksrc.tar'
             }
         }
 
@@ -42,24 +42,24 @@ pipeline {
         stage('Archive kernel image') {
             steps {
                 sh 'mv -v vmlinux vmlinux.64'
-                sh 'tar cvjf e100-kernel.tar.bz2 vmlinux.64'
-                archiveArtifacts artifacts: 'e100-kernel.tar.bz2', fingerprint: true, onlyIfSuccessful: true
+                sh 'tar cvjf e300-kernel.tar.bz2 vmlinux.64'
+                archiveArtifacts artifacts: 'e300-kernel.tar.bz2', fingerprint: true, onlyIfSuccessful: true
             }
         }
         
         stage('Archive kernel modules') {
             steps {
                 sh 'make ARCH=mips CROSS_COMPILE=mips64-octeon-linux- INSTALL_MOD_PATH=destdir modules_install'
-                sh 'tar cvjf e100-modules.tar.bz2 -C destdir .'
-                archiveArtifacts artifacts: 'e100-modules.tar.bz2', fingerprint: true, onlyIfSuccessful: true
+                sh 'tar cvjf e300-modules.tar.bz2 -C destdir .'
+                archiveArtifacts artifacts: 'e300-modules.tar.bz2', fingerprint: true, onlyIfSuccessful: true
             }
         }
 
         stage('Archive build tree') {
             steps {
-                sh 'tar -uvf e100-ksrc.tar Module.symvers'
-                sh 'bzip2 e100-ksrc.tar'
-                archiveArtifacts artifacts: 'e100-ksrc.tar.bz2', fingerprint: true, onlyIfSuccessful: true
+                sh 'tar -uvf e300-ksrc.tar Module.symvers'
+                sh 'bzip2 e300-ksrc.tar'
+                archiveArtifacts artifacts: 'e300-ksrc.tar.bz2', fingerprint: true, onlyIfSuccessful: true
             }
         }
     }